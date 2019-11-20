5fe62f9138a4032ab71aabd84249afc6c2e1c0b8
oheast <oheast55@gmail.com>
2019-11-20 22:27:06 +0900

    update

--- datasets/youtube_audio_downloader.py
+++ datasets/youtube_audio_downloader.py
@@ -5,6 +5,17 @@ import argparse
 import json
 import youtube_dl
 from pytube import Playlist
+from pydub import AudioSegment
+from glob import glob
+
+def file_size(file_path):
+    """
+    this function will return the file size
+    """
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        size = file_info.st_size / (1024.0*1024.0) # byte to MB
+        return size
 
 def get_url_list(playlist_url):
     playlist = Playlist(playlist_url)
@@ -45,6 +56,18 @@ def extract_audio(folder, url_list):
                 print(e)
                 pass
 
+def fileSize_checker(audio_paths):
+    for path in audio_paths:
+        size = file_size(path)
+        print('========{} : {} MB'.format(path, size))
+        if file_size(path) >= 100.0:
+            raw_audio = AudioSegment.from_wav(path)
+            length = raw_audio.duration_seconds
+            audio_1 = raw_audio[:(length/2*1000)]
+            audio_2 = raw_audio[(length/2*1000):]
+            audio_1.export(path.replace('.wav', '_1.wav'), format='wav')
+            audio_2.export(path.replace('.wav', '_2.wav'), format='wav')
+            os.remove(path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="youtube video to wav. ")
@@ -71,3 +94,6 @@ if __name__ == '__main__':
             # 2. extract audio file from youtube video
             #    and save audio file in folder
             extract_audio(folder, url_list)
+            # 3. file size check
+            audio_paths = glob(folder + '/audio/*.wav')
+            fileSize_checker(audio_paths)
