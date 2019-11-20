9b5965bf875a79d5b0d19ffe2be94c551464e016
oheast <oheast55@gmail.com>
2019-11-20 22:31:44 +0900

    data pp script

---
+++ data_preprocessing.sh
@@ -0,0 +1,4 @@
+python -m recognition.watson --audio_pattern "./datasets/moon/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/park/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/son/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/yuinna/audio/*.wav"
---
+++ datasets/download_youtube_audio.sh
@@ -0,0 +1,7 @@
+python youtube_audio_downloader.py --url_type=playlist --json_filename=playlist.json
+python youtube_audio_downloader.py --url_type=video --json_filename=videolist.json
+
+python -m recognition.watson --audio_pattern "./datasets/moon/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/park/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/son/audio/*.wav"
+python -m recognition.watson --audio_pattern "./datasets/yuinna/audio/*.wav"
